b8747775ba5ebc93666ec865ceeff5fe9d3964e8
ElliotSalisbury <elliot.sal@gmail.com>
2015-11-09 12:14:44 -0800

    getAnnotations

--- Annotator/views.py
+++ Annotator/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from Annotator.models import Annotation
+from django.core import serializers
 
 def index(request):
-	return render_to_response('Annotator/index.html')
+    return render_to_response('Annotator/index.html')
+
+def getAnnotations(request):
+    data = serializers.serialize("json", Annotation.objects.all())
+    return HttpResponse(data, content_type="application/json")
