567f5ead671bcf5c3f78203ee84dd4cab2342347
ElliotSalisbury <elliot.sal@gmail.com>
2015-11-09 12:03:49 -0800

    index page added

--- CoLaborative/urls.py
+++ CoLaborative/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CoLaborative.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('Annotator.urls', namespace="annotator")),
     url(r'^admin/', include(admin.site.urls)),
+
 )
