cb78dc8fd3b8ccb8c99ad431fc64eed272a90cb0
ElliotSalisbury <elliot.sal@gmail.com>
2015-11-09 14:07:17 -0800

    start polling every second

--- Annotator/static/Annotator/index.js
+++ Annotator/static/Annotator/index.js
@@ -1,9 +1,13 @@
 var image_url = 'https://cs.stanford.edu/people/rak248/VG_100K/2349753.jpg';
 var ctx = myCanvas.getContext('2d');
 var img = new Image;
-img.onload = function(){
+img.onload = function() {
   ctx.drawImage(img,0,0,1000,1000); // Or at whatever offset you like
 };
 img.src = image_url;
 
 
+//start pulling annotations from server every second
+$(function() {
+  setInterval(getAnnotations, 1000);
+});
