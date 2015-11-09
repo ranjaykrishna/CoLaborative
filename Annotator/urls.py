3366d6830c0d9977fd73c4982b01feec33aa635e
ElliotSalisbury <elliot.sal@gmail.com>
2015-11-09 12:24:46 -0800

    editAnnotation

--- Annotator/urls.py
+++ Annotator/urls.py
@@ -4,4 +4,6 @@ from Annotator import views
 urlpatterns = patterns('',
 					   #/
 					   url(r'^$', views.index, name='index'),
+                       url(r'^$', views.getAnnotations, name='getAnnotations'),
+                       url(r'^$', views.editAnnotation, name='editAnnotation'),
 )
