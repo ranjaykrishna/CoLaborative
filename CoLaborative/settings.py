5741a3b48c3b714225d985e0ef76265ead11e44f
Ranjay Krishna <rak248@cornell.edu>
2015-11-09 15:37:00 -0800

    adding more code for heroku

--- CoLaborative/settings.py
+++ CoLaborative/settings.py
@@ -101,5 +101,6 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-        )
+  os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- CoLaborative/wsgi.py
+++ CoLaborative/wsgi.py
@@ -12,5 +12,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CoLaborative.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 #application = Cling(get_wsgi_application())
