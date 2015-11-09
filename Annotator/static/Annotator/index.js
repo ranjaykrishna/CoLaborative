0bd468b48ed35848f589ce697c0049944a9ed69b
Ranjay Krishna <rak248@cornell.edu>
2015-11-09 13:07:42 -0800

    adding basic canvas displaying image

--- Annotator/static/Annotator/index.js
+++ Annotator/static/Annotator/index.js
@@ -1,3 +1,9 @@
 var image_url = 'https://cs.stanford.edu/people/rak248/VG_100K/2349753.jpg';
+var ctx = myCanvas.getContext('2d');
+var img = new Image;
+img.onload = function(){
+  ctx.drawImage(img,0,0,1000,1000); // Or at whatever offset you like
+};
+img.src = image_url;
 
 
